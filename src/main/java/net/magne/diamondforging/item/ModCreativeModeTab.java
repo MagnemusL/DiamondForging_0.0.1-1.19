69c26dfaa3c4cf7aac538ff09184cef47f72c574
MagnemusL <magnelindholm@outlook.dk>
2023-05-18 17:01:09 +0200

    Added an new custom tab named "Diamond Forging Tab" and put 'Diamond Plate', and 'Diamond Rod' in the tab.

---
+++ src/main/java/net/magne/diamondforging/item/ModCreativeModeTab.java
@@ -0,0 +1,13 @@
+package net.magne.diamondforging.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class ModCreativeModeTab {
+    public static final CreativeModeTab DIAMONDFORGING_TAB = new CreativeModeTab("diamondtab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.DIAMOND_PLATE.get());
+        }
+    };
+}
--- src/main/java/net/magne/diamondforging/item/ModItems.java
+++ src/main/java/net/magne/diamondforging/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.magne.diamondforging.item;
 
 import net.magne.diamondforging.DiamondForging;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,9 +12,9 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, DiamondForging.MOD_ID);
 
     public static final RegistryObject<Item> DIAMOND_PLATE = ITEMS.register("diamond_plate",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.DIAMONDFORGING_TAB)));
     public static final RegistryObject<Item> DIAMOND_ROD = ITEMS.register("diamond_rod",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.DIAMONDFORGING_TAB)));
 
 
     public static void register(IEventBus eventBus) {
