b58ce59d2eaeba470dbd66b035558e58e9cd74a7
MagnemusL <magnelindholm@outlook.dk>
2023-05-20 12:59:01 +0200

    Added a LOT of things.

--- src/main/java/net/magne/diamondforging/item/ModItems.java
+++ src/main/java/net/magne/diamondforging/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.magne.diamondforging.item;
 
 import net.magne.diamondforging.DiamondForging;
+import net.magne.diamondforging.item.custom.EightBallItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -19,6 +20,26 @@ public class ModItems {
 
     public static final RegistryObject<Item> GLOWING_DIAMOND_SHEET = ITEMS.register("glowing_diamond_sheet",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.DIAMONDFORGING_TAB)));
+
+    public static final RegistryObject<Item> GLOWING_DIAMOND_ROD = ITEMS.register("glowing_diamond_rod",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.DIAMONDFORGING_TAB)));
+
+    public static final RegistryObject<Item> DIAMOND_WIRE = ITEMS.register("diamond_wire",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.DIAMONDFORGING_TAB)));
+
+    public static final RegistryObject<Item> GLOWING_DIAMOND_WIRE = ITEMS.register("glowing_diamond_wire",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.DIAMONDFORGING_TAB)));
+
+    public static final RegistryObject<Item> DIAMOND_SPHERE = ITEMS.register("diamond_sphere",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.DIAMONDFORGING_TAB)));
+
+    public static final RegistryObject<Item> GLOWING_DIAMOND_SPHERE = ITEMS.register("glowing_diamond_sphere",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.DIAMONDFORGING_TAB)));
+
+    public static final RegistryObject<Item> GLOWING_DIAMOND = ITEMS.register("glowing_diamond",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.DIAMONDFORGING_TAB)));
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativeModeTab.DIAMONDFORGING_TAB).stacksTo(1)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
